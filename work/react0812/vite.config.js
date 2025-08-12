7a06d9f304cbf02b5917ebc700e22fbde7f68b72
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-08-12 14:15:02 +0900

    Update vite.config.js

--- work/react0812/vite.config.js
+++ work/react0812/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react2508/react0812/',
+  base: '/react2508/work/react0812/',
   plugins: [react()],
 })
